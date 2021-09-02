3b5a404b753ede175f0e0b505c6d1f53937c3e78
g0getter <84381320+g0getter@users.noreply.github.com>
2021-09-02 20:39:57 +0900

    Revert "bold, italic, underlined, kern, highlight 등 (#4)"
    This reverts commit 5923e886ad6e6da2c0b355fa1a9b969449fe653e.

--- Extensions/ColorExtensions/Color.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Color.swift
-//  Extensions
-//
-//  Created by 여나경 on 2021/09/02.
-//
-
-import Foundation
--- Extensions/ViewController.swift
+++ Extensions/ViewController.swift
@@ -9,18 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var textLabel: UILabel!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        textLabel.attributedText = NSMutableAttributedString()
-            .bold("Bold\n", fontSize: 20)
-            .regular("Regular\n", fontSize: 25)
-            .underlined("Underlined\n", fontSize: 30)
-            .italic("Italic\n", fontSize: 33)
-            .highlight("Highlight\n", fontSize: 34, highlightColor: .purple)
-            .highlight("Orange\n", fontSize: 35, highlightColor: .orange)
-            .kern("Kern\n", fontSize: 45, kernValue: 30)
+        // Do any additional setup after loading the view.
     }
 
 
