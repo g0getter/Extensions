70c708e6787a01dedb169d63820a4c51618aecce
g0getter <84381320+g0getter@users.noreply.github.com>
2021-09-04 21:39:32 +0900

    [NSMutableAttributedString] 글자색, 줄간격, image 삽입 (#7)

--- Extensions/StringExtensions/NSMutableAttributedString.swift
+++ Extensions/StringExtensions/NSMutableAttributedString.swift
@@ -35,41 +35,71 @@ extension NSMutableAttributedString {
     }
     
     /// 밑줄
-    func underlined(_ value:String, fontSize: CGFloat) -> NSMutableAttributedString {
+    func underlined(_ value: String, fontSize: CGFloat) -> NSMutableAttributedString {
         let font = UIFont.systemFont(ofSize: fontSize)
         let attributes:[NSAttributedString.Key : Any] = [
             .font: font,
             .underlineStyle : NSUnderlineStyle.single.rawValue
         ]
-        
         self.append(NSAttributedString(string: value, attributes:attributes))
         return self
     }
     
     // MARK: - 여백
     /// 자간
-    func kern(_ value:String, fontSize: CGFloat, kernValue: CGFloat) -> NSMutableAttributedString {
+    func kern(_ value: String, fontSize: CGFloat, kernValue: CGFloat) -> NSMutableAttributedString {
         let font = UIFont.systemFont(ofSize: fontSize)
         let attributes:[NSAttributedString.Key : Any] = [
             .font: font,
             .kern: kernValue,            
         ]
-        
         self.append(NSAttributedString(string: value, attributes:attributes))
         return self
     }
     
-    // MARK: - Highlight
-    /// Highlight with `highlightColor`. foregroundColor is white
-    func highlight(_ value:String, fontSize: CGFloat, highlightColor: UIColor) -> NSMutableAttributedString {
+    // MARK: - Color
+    /// 글자색 지정
+    func colored(_ value: String, fontSize: CGFloat, fontColor: UIColor) -> NSMutableAttributedString {
+        let font = UIFont.systemFont(ofSize: fontSize)
+        let attributes:[NSAttributedString.Key : Any] = [
+            .font: font,
+            .foregroundColor: fontColor,
+        ]
+        self.append(NSAttributedString(string: value, attributes:attributes))
+        return self
+    }
+    
+    /// `highlightColor`색으로 하이라이트 표시. 글자 색(foregroundColor)는 흰색.
+    func highlight(_ value: String, fontSize: CGFloat, highlightColor: UIColor) -> NSMutableAttributedString {
         let font = UIFont.systemFont(ofSize: fontSize)
         let attributes:[NSAttributedString.Key : Any] = [
             .font: font,
             .foregroundColor: UIColor.white,
             .backgroundColor: highlightColor,
         ]
-        
         self.append(NSAttributedString(string: value, attributes:attributes))
+        return self
+    }
+    
+    // MARK: - LineSpace
+    /// 줄간격 설정. `value`의 아래쪽 간격이 설정됨.
+    func lineSpaced(_ value: String, _ spacing: CGFloat) -> NSMutableAttributedString {
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = spacing
+        let attributedString = NSAttributedString(string: value, attributes: [NSAttributedString.Key.paragraphStyle: paragraphStyle])
+        self.append(attributedString)
+        return self
+    }
+    
+    // MARK: - Image 삽입
+    /// Inserts an image into an attributed string. <- using `NSTextAttachment`
+    // TODO: 높이(크기) 설정. 폰트와 같은 높이로?
+    func appendImage(_ image: UIImage?) -> NSMutableAttributedString {
+        guard image != nil else { return self }
+        let imageAttachment: NSTextAttachment = NSTextAttachment()
+        imageAttachment.image = image
+        let string: NSAttributedString = NSAttributedString(attachment: imageAttachment)
+        self.append(string)
         return self
     }
 }
